db93d678d3d96ae4bf33bebf745ae3a80ddd9997
ccastelot <ccastelot60@gmail.com>
2013-12-21 09:50:44 -0500

    Homework Assignment 1 Completed

--- iTunesU/Matchismo/Matchismo/CardGameViewController.m
+++ iTunesU/Matchismo/Matchismo/CardGameViewController.m
@@ -7,36 +7,57 @@
 //
 
 #import "CardGameViewController.h"
+#import "Card.h"
+#import "Deck.h"
+#import "PlayingCard.h"
+#import "PlayingCardDeck.h" 
 
 @interface CardGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-
+@property (strong, nonatomic) Deck *cardDeck;
 @end
 
 @implementation CardGameViewController
 
+-(void)setCardDeck:(Deck *)cardDeck{
+    if (!cardDeck) {
+        _cardDeck = [[PlayingCardDeck alloc]init];
+        NSLog(@"Card Count: %d", [self.cardDeck.cards count]);
+    }
+    
+}
+
 -(void)setFlipCount:(int)flipCount{
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-    NSLog(@"FlipCount changed to %d", self.flipCount);
+    NSLog(@"FlipCount: %d", self.flipCount);
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender {
-    
-    if([sender.currentTitle length]){
-        [sender setBackgroundImage:[UIImage imageNamed:@"backImage"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@""
-                forState:UIControlStateNormal];
-    } else{
-        [sender setBackgroundImage:[UIImage imageNamed:@"frontImage"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎"
-                forState:UIControlStateNormal];
-    }
     self.flipCount++;
+    if(!_cardDeck){
+        [self setCardDeck:_cardDeck];
+    }
+    if ([self.cardDeck.cards count] > 0){
+        if([sender.currentTitle length]){
+            [sender setBackgroundImage:[UIImage imageNamed:@"backImage"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:@""
+                    forState:UIControlStateNormal];
+        } else{
+            Card *card = [[self cardDeck] drawRandomCard];
+            [sender setBackgroundImage:[UIImage imageNamed:@"frontImage"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:card.contents
+                    forState:UIControlStateNormal];
+        }
+        NSLog(@"\tCards Remain in Deck: %d", [self.cardDeck.cards count]);
+    }
+    else{
+        self.flipsLabel.text = [NSString stringWithFormat:@"No More Cards to Flip: %d", self.flipCount];
+    }
 }
 
 @end
